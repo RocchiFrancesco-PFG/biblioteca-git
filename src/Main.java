b929522b808d37fbcb75242ce36f26be88404fa1
Prof. Francesco Rocchi <francesco.rocchi@polofermigiorgi.it>
2026-02-18 11:17:40 +0100

    modificato messaggio di benvenuto

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Benvenuti nella biblioteca!");
+        System.out.println("Benvenuti nella biblioteca del Prof. Rocchi!");
 
         // --- Costruzione catalogo demo --------------------------------
         Catalogo catalogo = new Catalogo();
